0101bdd73468a676468566b7260f29da56273c21
Vincent Romero <vincent.romero@ticketmaster.com>
2019-04-11 03:54:37 -0700

    3. Define action types and action creators — AND Maybe? Action Creators with Side Effects?

---
+++ src/redux/actions/actions.jsx
@@ -0,0 +1,88 @@
+
+// other constants
+export const ViewFilters = {
+    SHOW_MENU: 'SHOW_MENU',
+    SHOW_CHANGE_PASSWORD: 'SHOW_CHANGE_PASSWORD'
+}
+
+export const MenuViewFilters = {
+    SHOW_DEFAULT: 'SHOW_DEFAULT',
+    SHOW_LATEST: 'SHOW_LATEST'
+}
+
+export const ChangePasswordViewFilters = {
+    SHOW_FORM: 'SHOW_FORM',
+    SHOW_SUCCESS_MESSAGE: 'SHOW_SUCCESS_MESSAGE'
+}
+
+// types
+export const CLOSE_DROPDOWN = 'CLOSE_DROPDOWN'
+export const TOGGLE_DROPDOWN = 'TOGGLE_DROPDOWN'
+export const SET_VIEW_FILTER = 'SET_VIEW_FILTER'
+
+
+export const SET_MENU_VIEW_FILTER = 'SET_MENU_VIEW_FILTER'
+export const SELECT_MENU_ITEM = 'SELECT_MENU_ITEM'
+
+export const SET_CHANGE_PASSWORD_VIEW_FILTER = 'SET_CHANGE_PASSWORD_VIEW_FILTER'
+export const RESET_CHANGE_PASSWORD_FORM = 'RESET_CHANGE_PASSWORD_FORM'
+export const CHANGE_PASSWORD_SENT = 'CHANGE_PASSWORD_SENT'
+export const CHANGE_PASSWORD_FAILED = 'CHANGE_PASSWORD_FAILED'
+export const CHANGE_PASSWORD_SUCCESS = 'CHANGE_PASSWORD_SUCCESS'
+
+// creators
+export const toggleDropdown = ( isVisible = false ) => ({
+    type: TOGGLE_DROPDOWN,
+    isVisible
+})
+
+export const setViewFilter = ( filter = ViewFilters.SHOW_MENU ) => ({
+    type: SET_VIEW_FILTER,
+    filter
+})
+
+export const setMenuViewFilter = ( filter = MenuViewFilters.SHOW_DEFAULT ) => ({
+    type: SET_MENU_VIEW_FILTER,
+    filter
+})
+
+export const selectMenutItem = (item) => ({
+    type: SELECT_MENU_ITEM,
+    item
+})
+
+export const setChangePasswordViewFilter = ( filter = MenuViewFilters.SHOW_DEFAULT ) => ({
+    type: SET_CHANGE_PASSWORD_VIEW_FILTER,
+    filter
+})
+
+export const resetChangePasswordForm = () => ({
+    type: RESET_CHANGE_PASSWORD_FORM
+})
+
+// sync actions
+export const changePasswordSent = () => ({
+    type: CHANGE_PASSWORD_SENT
+})
+
+export const changePasswordFailed = (error) => ({
+    type: CHANGE_PASSWORD_FAILED,
+    error
+})
+
+export const changePasswordSuccess = () => ({
+    type: CHANGE_PASSWORD_SUCCESS
+})
+
+// async action creator
+export function changePassword(oldPasssword, newPassword) { 
+    // @todo must use Promise to resolve request
+    return changePasswordSuccess
+}
+
+export function closeDropdown() {
+    // use thunk
+    // @todo must dispatch action to reset Menu View and Passwod View
+    
+}
+
